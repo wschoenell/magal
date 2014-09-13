857e85f92c55bfa00eb7c388576d7b63058d7e02
William Schoenell <wschoenell@gmail.com>
2014-09-13 02:33:38 +0200

    Preparing distribution. Moving scripts to magal/scripts/ directory.

--- src/magal/scripts/magal_library.py
+++ src/magal/scripts/magal_library.py
@@ -24,7 +24,7 @@ from magal.util.cosmo import zcor
 from magal.photometry.syntphot import spec2filterset
 
 
-def spec2filter_z(args):  #FIXME: Move this to somewhere else
+def spec2filter_z(args):  # FIXME: Move this to somewhere else
     spec, ccd_filter, z = args
     n_spec = np.shape(spec)[0]
 
@@ -49,128 +49,132 @@ def spec2filter_z(args):  #FIXME: Move this to somewhere else
     return x
 
 
-#if __name__ == '__main__':
+def main():
+    if len(sys.argv) < 2:
+        print 'Usage: %s configuration.ini' % sys.argv[0]
+        return 1
 
-#### ALL the configfile variables are loaded here ####
-#TODO: Put some checking/error handling here.
-config = ConfigParser.ConfigParser()
-config.read(sys.argv[1])
-libfile = os.path.expandvars(config.get('LibraryGeneral', 'library_file'))
-library_type = os.path.expandvars(config.get('LibraryGeneral', 'library_type'))
-filter_file = os.path.expandvars(config.get('LibraryGeneral', 'filter_file'))
+    # ### ALL the configfile variables are loaded here ####
+    #TODO: Put some checking/error handling here.
+    config = ConfigParser.ConfigParser()
+    config.read(sys.argv[1])
+    libfile = os.path.expandvars(config.get('LibraryGeneral', 'library_file'))
+    library_type = os.path.expandvars(config.get('LibraryGeneral', 'library_type'))
+    filter_file = os.path.expandvars(config.get('LibraryGeneral', 'filter_file'))
 
-filter_id = config.get('LibraryGeneral', 'filterset')  #TODO: Check if it is valid.
-z_from = config.getfloat('LibraryGeneral', 'z_from')
-z_to = config.getfloat('LibraryGeneral', 'z_to')
-z_step = config.getfloat('LibraryGeneral', 'z_step')
-try:
-    cosmology = ast.literal_eval(config.get('LibraryGeneral', 'cosmology'))
-    from astropy.cosmology import FlatLambdaCDM
-    cosmo = FlatLambdaCDM(H0=cosmology['H0'], Om0=cosmology['Om0'])
-except NoOptionError:
-    print 'Adopting the WMAP9 cosmology:'
-    from astropy.cosmology import WMAP9 as cosmo
-
-    print cosmo.__doc__
-
-try:
-    allow_overwrite = config.getboolean('LibraryGeneral', 'allow_overwrite')
-except NoOptionError:
-    allow_overwrite = False
-if os.path.exists(libfile):
-    if allow_overwrite:
-        try:
-            os.unlink(libfile)
-        except:
-            print u'Cannot DELETE file {0:s}.'.format(libfile)
-            sys.exit(1)
-    else:
-        print u'File {0:s} already exists.'.format(libfile)
-        sys.exit(1)
-
-
-# 1 - Two-exponential library parameters:
-if library_type == 'two_exp':
-    inp_file = os.path.expandvars(config.get('LibraryParameters', 'input_file'))
-    basedir = os.path.expandvars(config.get('LibraryParameters', 'bases_dir'))
-    basefile = os.path.expandvars(config.get('LibraryParameters', 'base_file'))
-    fraction_type = config.get('LibraryParameters', 'fraction_type')  # light or mass
+    filter_id = config.get('LibraryGeneral', 'filterset')  #TODO: Check if it is valid.
+    z_from = config.getfloat('LibraryGeneral', 'z_from')
+    z_to = config.getfloat('LibraryGeneral', 'z_to')
+    z_step = config.getfloat('LibraryGeneral', 'z_step')
     try:
-        lambda_norm = config.getfloat('LibraryParameters', 'lambda_norm')  # mass normalization wl
+        cosmology = ast.literal_eval(config.get('LibraryGeneral', 'cosmology'))
+        from astropy.cosmology import FlatLambdaCDM
+
+        cosmo = FlatLambdaCDM(H0=cosmology['H0'], Om0=cosmology['Om0'])
     except NoOptionError:
-        lambda_norm = 4020
-    LibModel = LibraryModel(library_type, inp_file, basedir, basefile, fraction_type, lambda_norm)  # Init
-# 2 - STARLIGHT library parameters:
-elif library_type == 'starlight_sdss':
-    type = 'starlight_sdss'
-    inp_file = os.path.expandvars(config.get('LibraryParameters', 'input_file'))
-    tables_dir = os.path.expandvars(config.get('LibraryParameters', 'tables_dir'))
-    input_dir = os.path.expandvars(config.get('LibraryParameters', 'input_dir'))
-    output_dir = os.path.expandvars(config.get('LibraryParameters', 'output_dir'))
-    LibModel = LibraryModel(type, inp_file, tables_dir, input_dir, output_dir, cosmo)
+        print 'Adopting the WMAP9 cosmology:'
+        from astropy.cosmology import WMAP9 as cosmo
 
-#### -- ####
+        print cosmo.__doc__
 
-# 0 - Library Parameters
-z_range = np.arange(z_from, z_to, z_step)
-Nz = len(z_range)
+    try:
+        allow_overwrite = config.getboolean('LibraryGeneral', 'allow_overwrite')
+    except NoOptionError:
+        allow_overwrite = False
+    if os.path.exists(libfile):
+        if allow_overwrite:
+            try:
+                os.unlink(libfile)
+            except:
+                print u'Cannot DELETE file {0:s}.'.format(libfile)
+                return 1
+        else:
+            print u'File {0:s} already exists.'.format(libfile)
+            return 1
 
-# 1 - Init/Read Files
-## 1.1 - Load filter information
-f = FilterSet(filter_file)
-## 1.2 - New Library
-db = h5py.File(libfile, 'w')
 
-# Write full .ini file on model outputfile.
-aux_ini = open(sys.argv[1], 'r').read()
-ds = db.create_dataset('/ini_file', shape=(1,), dtype=h5py.new_vlen(str))
-ds[:] = aux_ini
+    # 1 - Two-exponential library parameters:
+    if library_type == 'two_exp':
+        inp_file = os.path.expandvars(config.get('LibraryParameters', 'input_file'))
+        basedir = os.path.expandvars(config.get('LibraryParameters', 'bases_dir'))
+        basefile = os.path.expandvars(config.get('LibraryParameters', 'base_file'))
+        fraction_type = config.get('LibraryParameters', 'fraction_type')  # light or mass
+        try:
+            lambda_norm = config.getfloat('LibraryParameters', 'lambda_norm')  # mass normalization wl
+        except NoOptionError:
+            lambda_norm = 4020
+        LibModel = LibraryModel(library_type, inp_file, basedir, basefile, fraction_type, lambda_norm)  # Init
+    # 2 - STARLIGHT library parameters:
+    elif library_type == 'starlight_sdss':
+        type = 'starlight_sdss'
+        inp_file = os.path.expandvars(config.get('LibraryParameters', 'input_file'))
+        tables_dir = os.path.expandvars(config.get('LibraryParameters', 'tables_dir'))
+        input_dir = os.path.expandvars(config.get('LibraryParameters', 'input_dir'))
+        output_dir = os.path.expandvars(config.get('LibraryParameters', 'output_dir'))
+        LibModel = LibraryModel(type, inp_file, tables_dir, input_dir, output_dir, cosmo)
 
-### 1.2.1 - Tables group
-db.create_group('/tables/')
-### 1.2.2 - Filtersystem group.
-db.create_group('/%s/' % filter_id)
-### 1.2.3 - CCD groups
-ccd_vec = {}
-db_vec = {}
-for ccd in f.filtersets[filter_id]:
-    f.load(filter_id, ccd)  # Load CCD filters.
-    filter_wls = f.filter_wls  # Get the wls
-    Nl = len(filter_wls)
-    db.create_group('/%s/%s/' % (filter_id, ccd))  # Create group to the CCD.
+    #### -- ####
 
-    ccd_vec.update({ccd: f.filterset})
+    # 0 - Library Parameters
+    z_range = np.arange(z_from, z_to, z_step)
+    Nz = len(z_range)
 
-    db.create_dataset(name='/%s/%s/filtercurves' % (filter_id, ccd),
-                      data=f.filterset)  #np.array(f.filterset, dtype=([('ID_filter', '|S32'), ('wl', '<f4'), ('transm', '<f4')])))
-    db.create_dataset(name='/%s/%s/filterset' % (filter_id, ccd), data=filter_wls)
-    chunk_size = 1 * LibModel.lib_size * Nl  # chunk_size = 1 (z) * lib_size * n_filters
-    # chunk_z = int(chunk_size / (64*1024) ) + 1 # make chunk_size of about 64kb and > 1
-    chunk_z = 1  # print (Nz,lib_size, Nl), (chunk_z,lib_size,Nl)
-    db_m = db.create_dataset('/%s/%s/%s' % (filter_id, ccd, 'library'), shape=(Nz, LibModel.lib_size, Nl),
-                             chunks=(chunk_z, LibModel.lib_size, Nl),
-                             dtype=np.dtype([('m_ab', '<f4'), ('e_ab', '<f4')]))
-    db_vec.update({ccd: db_m})
-### 1.2.3 - redshift
-db.create_dataset(name='/tables/z', data=z_range)
-### 1.2.4 - properties
-prop = db.create_dataset(name='/tables/properties', data=LibModel.input_data)
+    # 1 - Init/Read Files
+    ## 1.1 - Load filter information
+    f = FilterSet(filter_file)
+    ## 1.2 - New Library
+    db = h5py.File(libfile, 'w')
 
-# 2 - RUN!
-for i_model in range(LibModel.lib_size):
-    if i_model % 100 == 0:
-        print 'Running i_model: %i' % i_model
+    # Write full .ini file on model outputfile.
+    aux_ini = open(sys.argv[1], 'r').read()
+    ds = db.create_dataset('/ini_file', shape=(1,), dtype=h5py.new_vlen(str))
+    ds[:] = aux_ini
+
+    ### 1.2.1 - Tables group
+    db.create_group('/tables/')
+    ### 1.2.2 - Filtersystem group.
+    db.create_group('/%s/' % filter_id)
+    ### 1.2.3 - CCD groups
+    ccd_vec = {}
+    db_vec = {}
     for ccd in f.filtersets[filter_id]:
-        args = []
-        spec = LibModel.get_model_spectrum(i_model)
-        for z in z_range:
-            args.append((spec, ccd_vec[ccd], z))
-        pool = Pool()
-        result = pool.map(spec2filter_z, args)
-        pool.close()
-        pool.join()
-        db_vec[ccd][:, i_model, :] = result
+        f.load(filter_id, ccd)  # Load CCD filters.
+        filter_wls = f.filter_wls  # Get the wls
+        Nl = len(filter_wls)
+        db.create_group('/%s/%s/' % (filter_id, ccd))  # Create group to the CCD.
 
-print 'Finished calculating magnintudes.'
+        ccd_vec.update({ccd: f.filterset})
 
-db.close()
+        db.create_dataset(name='/%s/%s/filtercurves' % (filter_id, ccd),
+                          data=f.filterset)  #np.array(f.filterset, dtype=([('ID_filter', '|S32'), ('wl', '<f4'), ('transm', '<f4')])))
+        db.create_dataset(name='/%s/%s/filterset' % (filter_id, ccd), data=filter_wls)
+        chunk_size = 1 * LibModel.lib_size * Nl  # chunk_size = 1 (z) * lib_size * n_filters
+        # chunk_z = int(chunk_size / (64*1024) ) + 1 # make chunk_size of about 64kb and > 1
+        chunk_z = 1  # print (Nz,lib_size, Nl), (chunk_z,lib_size,Nl)
+        db_m = db.create_dataset('/%s/%s/%s' % (filter_id, ccd, 'library'), shape=(Nz, LibModel.lib_size, Nl),
+                                 chunks=(chunk_z, LibModel.lib_size, Nl),
+                                 dtype=np.dtype([('m_ab', '<f4'), ('e_ab', '<f4')]))
+        db_vec.update({ccd: db_m})
+    ### 1.2.3 - redshift
+    db.create_dataset(name='/tables/z', data=z_range)
+    ### 1.2.4 - properties
+    prop = db.create_dataset(name='/tables/properties', data=LibModel.input_data)
+
+    # 2 - RUN!
+    for i_model in range(LibModel.lib_size):
+        if i_model % 100 == 0:
+            print 'Running i_model: %i' % i_model
+        for ccd in f.filtersets[filter_id]:
+            args = []
+            spec = LibModel.get_model_spectrum(i_model)
+            for z in z_range:
+                args.append((spec, ccd_vec[ccd], z))
+            pool = Pool()
+            result = pool.map(spec2filter_z, args)
+            pool.close()
+            pool.join()
+            db_vec[ccd][:, i_model, :] = result
+
+    print 'Finished calculating magnintudes.'
+
+    db.close()
